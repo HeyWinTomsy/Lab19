be267fc668ccdc8bf81d293d105ac925e40bce3b
HeyWinTomsy <nazarantr@gmail.com>
2024-11-22 16:23:47 +0200

    Initial commit

---
+++ 1.py
@@ -0,0 +1,5 @@
+import csv
+import pandas as pd
+fname='production.csv'
+df=pd.read_csv(fname)
+print(df)
---
+++ 2.py
@@ -0,0 +1,12 @@
+import array
+import json
+ListCon=[]
+Month=("January","February","March","April","May","June","July","August","September","October","November","December")
+for i in range (len(Month)):
+    print(f"Введіть кількість беттоних плит виготовлених за {Month[i]}:")
+    c=int(input())
+    ListCon.append(c)
+Series=array.array('i',ListCon)
+Dict1=dict(zip(Month,ListCon))
+with open("Beton.json","w") as f:
+    json.dump(Dict1,f)
