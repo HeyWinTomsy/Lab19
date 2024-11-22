193a2daa3d6189eef9d108addb944aabefc2671b
HeyWinTomsy <nazarantr@gmail.com>
2024-11-22 17:02:52 +0200

    Initial commit

--- 2.py
+++ 2.py
@@ -1,4 +1,4 @@
-import array
+import pandas as pd
 import json
 ListCon=[]
 Month=("January","February","March","April","May","June","July","August","September","October","November","December")
@@ -6,7 +6,9 @@ for i in range (len(Month)):
     print(f"Введіть кількість беттоних плит виготовлених за {Month[i]}:")
     c=int(input())
     ListCon.append(c)
-Series=array.array('i',ListCon)
 Dict1=dict(zip(Month,ListCon))
+m1=pd.Series(Dict1)
 with open("Beton.json","w") as f:
-    json.dump(Dict1,f)
+    for keys,values in m1.items():
+        json.dump(keys,f)
+        json.dump(values,f)
